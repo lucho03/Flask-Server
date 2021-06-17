eb5ba678b26de3de013f7412065e773fea0208e6
lucho03 <l.valentinov.ivanov@gmail.com>
2021-06-17 22:58:43 +0300

    Add message class

--- classes.py
+++ classes.py
@@ -1,11 +1,18 @@
 class User():    
-    def add(self, username, enemy, num):
+    def add(self, username, enemy, num, ip):
         self.username=username
         self.num=num
         self.enemy=enemy
+        self.ip=ip
     
     def print(self):
         return f"Name: {self.username}"
     
     def __str__(self):
         return "Player " + str(self.num) + ": " + self.username + " vs " + self.enemy
+
+class Message():
+    def add(self, sender, receiver):
+        self.sender=sender
+        self.receiver=receiver
+
