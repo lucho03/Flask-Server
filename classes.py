b379e132f8eed15fc600076019e3b1ca67f0e2d9
lucho03 <l.valentinov.ivanov@gmail.com>
2021-06-19 23:38:55 +0300

    Class

--- classes.py
+++ classes.py
@@ -1,15 +1,19 @@
 class User():    
+    def __init__(self):
+        self.enemy="There is no enemy "
+    
     def add(self, username, enemy, num, ip):
         self.username=username
         self.num=num
-        self.enemy=enemy
+        if len(enemy) > 0:
+            self.enemy=enemy
         self.ip=ip
     
     def print(self):
         return f"Name: {self.username}"
     
     def __str__(self):
-        return "Player " + str(self.num) + ": " + self.username + " vs " + self.enemy
+        return "Player " + str(self.num) + ": " + self.username + '\r' + "Enemy: " + self.enemy
 
 class Message():
     def add(self, sender, receiver):
